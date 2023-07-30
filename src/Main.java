405177b0a120878680236409b196326dddbffe02
Tyunin Pavel <tuninpavel@gmail.com>
2023-07-30 13:01:06 +0600

    task done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import Classes.Toy;
+import Classes.ToyCollection;
+import Classes.ToyFactory;
+import Interfaces.iToy;
+import Interfaces.iToyFactory;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        iToyFactory toyFactory = new ToyFactory();
+        ArrayList<iToy> toys = new ArrayList<iToy>();
+        toys.add(toyFactory.createToy(1, "конструктор", 2));
+        toys.add(toyFactory.createToy(2, "робот", 2));
+        toys.add(toyFactory.createToy(3, "кукла", 6));
+        ToyCollection toyCollection = new ToyCollection(toys);
+        for (int i = 0; i < 10; i++){
+            //System.out.println(toyCollection.getToy());
+            toyCollection.writeToyToOutputFile(toyCollection.getToy());
+        }
     }
 }
